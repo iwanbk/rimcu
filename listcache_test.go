984db0bbb66f811e625d1df2cec81bb16376fd54
Iwan Budi Kusnanto <iwanbk@gmail.com>
2019-11-23 07:54:21 +0700

    Initial list cache with RESP2 implementation
    - improve list cache test: using real redis if available
    - implement notif buffering
    - update readme

--- listcache_test.go
+++ listcache_test.go
@@ -1,39 +1,248 @@
 package rimcu
 
 import (
+	"context"
+	"fmt"
+	"os"
 	"testing"
+	"time"
 
+	"github.com/alicebob/miniredis"
+	"github.com/gomodule/redigo/redis"
+	"github.com/rs/xid"
 	"github.com/stretchr/testify/require"
 )
 
-func TestListCache(t *testing.T) {
-	lc, err := newListCache(100)
-	require.NoError(t, err)
-	require.NotNil(t, lc)
+var (
+	listWaitDur = 2 * time.Second
+)
 
-	const (
-		key1 = "key1"
-		val1 = "val1"
-		val2 = "val2"
+// Test to make sure that push operation will not initiate in mem cache
+func TestListCachePushNotSetInMem(t *testing.T) {
+	lcs, cleanup := createListCacheResp2TestClient(t, 2)
+	defer cleanup()
+
+	lc1, lc2 := lcs[0], lcs[1]
+
+	var (
+		key1 = xid.New().String()
+		val1 = "val_1"
+		val2 = "val_2"
+		val3 = "val_3"
+		ctx  = context.Background()
 	)
 
-	val, ok := lc.Lpop(key1)
-	require.False(t, ok)
-	require.Empty(t, val)
+	err := lc1.Rpush(ctx, key1, val1)
+	require.NoError(t, err)
 
-	lc.Rpush(key1, val1)
-	lc.Rpush(key1, val2)
+	err = lc2.Rpush(ctx, key1, val2)
+	require.NoError(t, err)
 
-	val, ok = lc.Lpop(key1)
-	require.True(t, ok)
-	require.Equal(t, val1, val)
+	err = lc1.Rpush(ctx, key1, val3)
+	require.NoError(t, err)
 
-	val, ok = lc.Lpop(key1)
-	require.True(t, ok)
-	require.Equal(t, val2, val)
+	{
+		time.Sleep(listWaitDur)
 
-	val, ok = lc.Lpop(key1)
-	require.False(t, ok)
-	require.Empty(t, val)
+		cv, ok := lc1.memGet(key1)
+		require.False(t, ok)
+		require.Nil(t, cv)
 
+		_, ok = lc2.memGet(key1)
+		require.False(t, ok)
+		require.Nil(t, cv)
+	}
+
+}
+
+// Test RPUSH propagates to other node
+func TestListCachePushPropagate(t *testing.T) {
+	lcs, cleanup := createListCacheResp2TestClient(t, 2)
+	defer cleanup()
+
+	lc1, lc2 := lcs[0], lcs[1]
+
+	var (
+		key1 = xid.New().String()
+		val1 = "val_1"
+		val2 = "val_2"
+		val3 = "val_3"
+		ctx  = context.Background()
+	)
+
+	// GET to listen to the RPUSH event
+	{
+		val, err := lc1.Get(ctx, key1)
+		require.NoError(t, err)
+		require.NotNil(t, val)
+		require.Empty(t, val)
+
+		val, err = lc2.Get(ctx, key1)
+		require.NoError(t, err)
+		require.NotNil(t, val)
+		require.Empty(t, val)
+	}
+	// PUSH
+	{
+		err := lc1.Rpush(ctx, key1, val1)
+		require.NoError(t, err)
+
+		err = lc2.Rpush(ctx, key1, val2)
+		require.NoError(t, err)
+
+		err = lc1.Rpush(ctx, key1, val3)
+		require.NoError(t, err)
+	}
+
+	// check it is properly propagated
+	{
+		time.Sleep(listWaitDur)
+
+		expected := []string{val1, val2, val3}
+
+		cv, ok := lc1.memGet(key1)
+		require.True(t, ok)
+		require.Equal(t, expected, cv.GetList())
+
+		cv, ok = lc2.memGet(key1)
+		require.True(t, ok)
+		require.Equal(t, expected, cv.GetList())
+	}
+
+}
+
+func TestListCachePopNotSetInMem(t *testing.T) {
+	lcs, cleanup := createListCacheResp2TestClient(t, 2)
+	defer cleanup()
+
+	lc1, lc2 := lcs[0], lcs[1]
+
+	var (
+		key1 = xid.New().String()
+		val1 = "val_1"
+		val2 = "val_2"
+		ctx  = context.Background()
+	)
+
+	err := lc1.Rpush(ctx, key1, val1)
+	require.NoError(t, err)
+
+	err = lc2.Rpush(ctx, key1, val2)
+	require.NoError(t, err)
+
+	_, _, err = lc2.Lpop(ctx, key1)
+	require.NoError(t, err)
+
+	_, _, err = lc1.Lpop(ctx, key1)
+	require.NoError(t, err)
+
+	{
+		time.Sleep(listWaitDur)
+
+		cv, ok := lc1.memGet(key1)
+		require.False(t, ok)
+		require.Nil(t, cv)
+
+		_, ok = lc2.memGet(key1)
+		require.False(t, ok)
+		require.Nil(t, cv)
+	}
+
+}
+
+// Test LPOP propagates to other node
+func TestListCachePopPropagate(t *testing.T) {
+	lcs, cleanup := createListCacheResp2TestClient(t, 2)
+	defer cleanup()
+
+	lc1, lc2 := lcs[0], lcs[1]
+
+	var (
+		key1 = xid.New().String()
+		val1 = "val_1"
+		val2 = "val_2"
+		val3 = "val_3"
+		ctx  = context.Background()
+	)
+
+	// GET to listen to the RPUSH/LPOP event
+	{
+		_, err := lc1.Get(ctx, key1)
+		require.NoError(t, err)
+
+		_, err = lc2.Get(ctx, key1)
+		require.NoError(t, err)
+	}
+	// PUSH
+	{
+		err := lc1.Rpush(ctx, key1, val1)
+		require.NoError(t, err)
+
+		err = lc1.Rpush(ctx, key1, val2)
+		require.NoError(t, err)
+
+		err = lc1.Rpush(ctx, key1, val3)
+		require.NoError(t, err)
+	}
+
+	{ // POP
+		val, ok, err := lc2.Lpop(ctx, key1)
+		require.NoError(t, err)
+		require.True(t, ok)
+		require.Equal(t, val1, val)
+	}
+
+	// check it is properly propagated
+	{
+		time.Sleep(listWaitDur)
+
+		expected := []string{val2, val3}
+
+		cv, ok := lc1.memGet(key1)
+		require.True(t, ok)
+		require.Equal(t, expected, cv.GetList())
+
+		cv, ok = lc2.memGet(key1)
+		require.True(t, ok)
+		require.Equal(t, expected, cv.GetList())
+	}
+
+}
+func createListCacheResp2TestClient(t *testing.T, numCli int) ([]*listCache, func()) {
+	var (
+		caches     []*listCache
+		serverAddr = os.Getenv("TEST_REDIS_ADDR")
+		server     *miniredis.Miniredis
+		err        error
+	)
+
+	if serverAddr == "" { // if we don't have redis server for tests
+		server, err = miniredis.Run()
+		require.NoError(t, err)
+
+		time.Sleep(1 * time.Second) // miniredis sometime need startup time
+
+		serverAddr = server.Addr()
+	}
+
+	// set pool
+	for i := 0; i < numCli; i++ {
+		pool := &redis.Pool{
+			Dial: func() (redis.Conn, error) {
+				return redis.Dial("tcp", serverAddr)
+			},
+		}
+		cli, err := newListCache(pool, 1000, []byte(fmt.Sprintf("client_%d", i)))
+		require.NoError(t, err)
+		caches = append(caches, cli)
+	}
+
+	return caches, func() {
+		if server != nil {
+			server.Close()
+		}
+		//for _, cli := range caches {
+		//cli.Close()
+		//}
+	}
 }
---
+++ resp2_notif_subscriber.go
@@ -0,0 +1,104 @@
+package rimcu
+
+import (
+	"github.com/gomodule/redigo/redis"
+)
+
+type resp2NotifSubcriber struct {
+	pool              *redis.Pool
+	finishedCh        chan struct{}
+	logger            Logger
+	disconnectHandler func()
+	notifHandler      func([]byte)
+	channel           string
+}
+
+func newResp2NotifSubcriber(pool *redis.Pool, notifHandler func([]byte), disconnectHandler func(), channel string) *resp2NotifSubcriber {
+	return &resp2NotifSubcriber{
+		pool:              pool,
+		finishedCh:        make(chan struct{}),
+		logger:            &debugLogger{},
+		notifHandler:      notifHandler,
+		disconnectHandler: disconnectHandler,
+		channel:           channel,
+	}
+}
+
+func (lc *resp2NotifSubcriber) runSubscriber() error {
+	subscriberDoneCh, err := lc.startSub()
+	if err != nil {
+		return err
+	}
+	go func() {
+		for {
+			select {
+			case <-lc.finishedCh: // we are done
+				return
+			case <-subscriberDoneCh:
+				// we're just disconnected from our Notif channel,
+				// clear our in mem cache as we can't assume that the values
+				// still updated
+				lc.disconnectHandler()
+
+				// start new subscriber
+				subscriberDoneCh, err = lc.startSub()
+				if err != nil {
+					lc.logger.Errorf("failed to start subscriber: %v", err)
+				}
+			}
+		}
+	}()
+	return nil
+}
+
+// starts subscriber to listen to all of synchronization message sent by other nodes
+func (lc *resp2NotifSubcriber) startSub() (chan struct{}, error) {
+	doneCh := make(chan struct{})
+
+	// setup subscriber
+	sub, err := lc.subscribe(lc.channel)
+	if err != nil {
+		close(doneCh)
+		return doneCh, err
+	}
+
+	// we're just connected to our Notif channel,
+	// it means we previously not connected or disconnected from the Notif channel.
+	lc.disconnectHandler()
+
+	// run subscriber loop
+	go func() {
+		defer func() {
+			close(doneCh)
+			sub.Close()
+		}()
+
+		for {
+			switch v := sub.Receive().(type) {
+			case redis.Message:
+				lc.notifHandler(v.Data)
+			case error:
+				// TODO: don't log if it is not error, i.e.: on cache Close
+				lc.logger.Errorf("subscribe err: %v", v)
+				return
+			}
+		}
+	}()
+	return doneCh, nil
+}
+
+// subscribe to the notification channel
+func (lc *resp2NotifSubcriber) subscribe(channel string) (*redis.PubSubConn, error) {
+	conn := lc.pool.Get()
+	if err := conn.Err(); err != nil {
+		return nil, err
+	}
+	sub := &redis.PubSubConn{Conn: conn}
+
+	err := sub.Subscribe(channel)
+	if err != nil {
+		sub.Close()
+		return nil, err
+	}
+	return sub, nil
+}
