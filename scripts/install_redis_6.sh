d7b06ed4dc4d9b2a31e429fdf1387afa95772cd1
Iwan Budi Kusnanto <iwanbk@gmail.com>
2020-03-08 21:30:02 +0700

    fix redis version

--- scripts/install_redis_6.sh
+++ scripts/install_redis_6.sh
@@ -1,4 +1,5 @@
-wget -c https://github.com/antirez/redis/archive/unstable.tar.gz
-tar zxf unstable.tar.gz
-cd redis-unstable
+wget -c https://github.com/antirez/redis/archive/6.0-rc1.tar.gz
+tar zxf 6.0-rc1.tar.gz
+mv redis-6.0-rc1 redis
+cd redis
 make
